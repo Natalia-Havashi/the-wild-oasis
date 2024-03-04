33b2fe049bdf637771f0dd6e515f4cc4128208c0
Natalia-Havashi <natali.gavashi@gmail.com>
2024-03-04 19:06:14 +0200

    Logout

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,15 +1,16 @@
-import styled from "styled-components"
+import styled from "styled-components";
+import Logout from "../features/authentication/Logout";
 
 const StyledHeader = styled.header`
-    background-color: var(--color-grey-0);
-    padding: 1.2rem 4.8rem;
-    border-bottom: 1px solid var(--color-grey-100);
-`
+  background-color: var(--color-grey-0);
+  padding: 1.2rem 4.8rem;
+  border-bottom: 1px solid var(--color-grey-100);
+`;
 
-export default function Header(){
-    return (
-        <StyledHeader>
-            HEADER
-        </StyledHeader>
-    )
+export default function Header() {
+  return (
+    <StyledHeader>
+      <Logout />
+    </StyledHeader>
+  );
 }
