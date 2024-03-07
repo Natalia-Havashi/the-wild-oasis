cd68d3440002d0c44e66dfdb098671b8c3e900fb
Natalia-Havashi <natali.gavashi@gmail.com>
2024-03-07 18:49:52 +0200

    Fix bug

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -1,22 +1,23 @@
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import { HiArrowUpOnSquare } from "react-icons/hi2";
 
 import BookingDataBox from "./BookingDataBox";
+import { useMoveBack } from "../../hooks/useMoveBack";
+import { useBooking } from "./useBooking";
+import { useCheckout } from "../check-in-out/useCheckout";
+import useDeleteBooking from "./useDeleteBooking";
+
 import Row from "../../ui/Row";
 import Heading from "../../ui/Heading";
 import Tag from "../../ui/Tag";
 import ButtonGroup from "../../ui/ButtonGroup";
 import Button from "../../ui/Button";
 import ButtonText from "../../ui/ButtonText";
-
-import { useMoveBack } from "../../hooks/useMoveBack";
-import { useBooking } from "./useBooking";
+import Empty from '../../ui/Empty';
 import Spinner from "../../ui/Spinner";
-import { useNavigate } from "react-router-dom";
-import { HiArrowUpOnSquare } from "react-icons/hi2";
-import { useCheckout } from "../check-in-out/useCheckout";
 import Modal from "../../ui/Modal";
 import ConfirmDelete from "../../ui/ConfirmDelete";
-import useDeleteBooking from "./useDeleteBooking";
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -32,6 +33,7 @@ function BookingDetail() {
   const moveBack = useMoveBack();
 
   if (isLoading) return <Spinner />;
+if(!booking) return <Empty resourceName='booking'/>
 
   const { status, id: bookingId } = booking;
 
--- src/ui/Menus.jsx
+++ src/ui/Menus.jsx
@@ -86,6 +86,7 @@ function Toggle({ id }) {
   const { openId, close, open, setPosition } = useContext(MenusContext);
 
   function handleClick(e) {
+    e.stopPropagation();
     const rect = e.target.closest("button").getBoundingClientRect();
     setPosition({
       x: window.innerWidth - rect.width - rect.x,
@@ -101,20 +102,22 @@ function Toggle({ id }) {
 }
 
 function List({ id, children }) {
-  const { openId, position,close } = useContext(MenusContext);
-const ref = useOutsideClick(close)
+  const { openId, position, close } = useContext(MenusContext);
+  const ref = useOutsideClick(close, false);
   if (openId !== id) return null;
 
   return createPortal(
-    <StyledList $position={position} ref={ref}>{children}</StyledList>,
+    <StyledList $position={position} ref={ref}>
+      {children}
+    </StyledList>,
     document.body
   );
 }
 
 function Button({ children, icon, onClick }) {
-  const {close} = useContext(MenusContext);
+  const { close } = useContext(MenusContext);
 
-  function handleClick(){
+  function handleClick() {
     onClick?.();
     close();
   }
